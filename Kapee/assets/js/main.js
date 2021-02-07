df1808977d180b73092f66b5c55e4cf3097a10db
kamilhasanov <kamilah@code.edu.az>
2021-02-08 02:19:06 +0400

    Product, Category, Testimonial, Info sections created

--- Kapee/assets/js/main.js
+++ Kapee/assets/js/main.js
@@ -34,5 +34,78 @@ $(document).ready(function () {
             }
         })
     }
+     
+
+    // Products corusel start
+    if ($("#products").length) {
+        $('#products .slider .owl-carousel').owlCarousel({
+            dots: false,
+            loop: true,
+            nav: true,
+            mouseDrag: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+          })
+    }
+
+    $(".product-color").click(function(e){
+        e.preventDefault();
+        $(".product-color").removeClass("active").next().addClass("active");
+        
+    })
+     
+
+    // Category corusel start
+    if ($("#category").length) {
+        $('#category .category-content .owl-carousel').owlCarousel({
+            dots: false,
+            loop: false,
+            nav: true,
+            mouseDrag: true,
+            autoplay: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+    }
+    $( ".slider .owl-carousel .owl-prev").html('<i class="fas fa-angle-left"></i>');
+    $( ".slider .owl-carousel .owl-next").html('<i class="fas fa-angle-right"></i>');
+
+
+    // Customer corusel start
+    if ($("#customer").length) {
+        $('#customer .customer-testimonial .owl-carousel').owlCarousel({
+            items: 1,
+            dots: false,
+            loop: true,
+            nav: true,
+            mouseDrag: true,
+        })
+    }
+    // $( ".slider .owl-carousel .owl-prev").html('<i class="fas fa-angle-left"></i>');
+    // $( ".slider .owl-carousel .owl-next").html('<i class="fas fa-angle-right"></i>');
+
+    
 })
 
