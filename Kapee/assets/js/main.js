59b9faebfe070405a93e93a4debca8e88c6b4c1c
kamilhasanov <kamilah@code.edu.az>
2021-03-04 19:32:55 +0400

    Category detail all created

--- Kapee/assets/js/main.js
+++ Kapee/assets/js/main.js
@@ -538,7 +538,67 @@ $(document).ready(function () {
                 $(this).removeClass("fa-angle-up").addClass("fa-angle-down");
                 $(this).parent().next().next().addClass("d-none");
              
-              }
+            }
+        })
+        $("#main-page .color-filter .color-filter-list .color-filter-item a .product-color span").hover(function(){
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip()
+            })
+        })
+        $("#main-page .product-categories h2").click(function(){
+            console.log();
+            if($("#main-page .product-categories h2 i").hasClass("fa-angle-up")){
+                $("#main-page .product-categories h2 i").removeClass("fa-angle-up").addClass("fa-angle-down");
+                $(this).next().addClass("d-none");
+            }
+            else{
+                $("#main-page .product-categories h2 i").removeClass("fa-angle-down").addClass("fa-angle-up");
+                $(this).next().removeClass("d-none");
+            }
+        })
+        $("#main-page .price-filter h2").click(function(){
+            console.log();
+            if($("#main-page .price-filter h2 i").hasClass("fa-angle-up")){
+                $("#main-page .price-filter h2 i").removeClass("fa-angle-up").addClass("fa-angle-down");
+                $(this).next().addClass("d-none");
+            }
+            else{
+                $("#main-page .price-filter h2 i").removeClass("fa-angle-down").addClass("fa-angle-up");
+                $(this).next().removeClass("d-none");
+            }
+        })
+        $("#main-page .color-filter h2").click(function(){
+            console.log();
+            if($("#main-page .color-filter h2 i").hasClass("fa-angle-up")){
+                $("#main-page .color-filter h2 i").removeClass("fa-angle-up").addClass("fa-angle-down");
+                $(this).next().addClass("d-none");
+            }
+            else{
+                $("#main-page .color-filter h2 i").removeClass("fa-angle-down").addClass("fa-angle-up");
+                $(this).next().removeClass("d-none");
+            }
+        })
+        $("#main-page .size-filter h2").click(function(){
+            console.log();
+            if($("#main-page .size-filter h2 i").hasClass("fa-angle-up")){
+                $("#main-page .size-filter h2 i").removeClass("fa-angle-up").addClass("fa-angle-down");
+                $(this).next().addClass("d-none");
+            }
+            else{
+                $("#main-page .size-filter h2 i").removeClass("fa-angle-down").addClass("fa-angle-up");
+                $(this).next().removeClass("d-none");
+            }
+        })
+        $("#main-page .star-filter h2").click(function(){
+            console.log();
+            if($("#main-page .star-filter h2 i").hasClass("fa-angle-up")){
+                $("#main-page .star-filter h2 i").removeClass("fa-angle-up").addClass("fa-angle-down");
+                $(this).next().addClass("d-none");
+            }
+            else{
+                $("#main-page .star-filter h2 i").removeClass("fa-angle-down").addClass("fa-angle-up");
+                $(this).next().removeClass("d-none");
+            }
         })
  
 })
